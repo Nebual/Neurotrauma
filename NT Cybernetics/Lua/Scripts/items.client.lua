e31a18d555a911536a14c4bd2ca71f7d49734c0f
Nebual <ben1120@gmail.com>
2024-08-21 01:45:52 -0700

    Fix performance regression of IsDead patch

--- NT Cybernetics/Lua/Scripts/items.client.lua
+++ NT Cybernetics/Lua/Scripts/items.client.lua
@@ -8,21 +8,25 @@ local allowedNecromancyItems = {
 	fpgacircuit = 1,
 }
 local temporarilyUndeadCharacter = nil
+local function patchIsDead()
+	-- this patch is performance intensive, so we only apply it while the specific necromancy is needed
+	Hook.Patch("NTC.IsDead_Patch", "Barotrauma.Character", "get_IsDead", function (instance, ptable)
+		if temporarilyUndeadCharacter ~= nil and temporarilyUndeadCharacter == instance then
+			ptable.PreventExecution = true
+			return false
+		end
+	end, Hook.HookMethodType.Before)
+end
+
 Hook.Patch("Barotrauma.CharacterHealth", "OnItemDropped", function (instance, ptable)
 	if instance.Character.IsDead and allowedNecromancyItems[ptable["item"].Prefab.Identifier.Value] ~= nil then
 		temporarilyUndeadCharacter = instance.Character
+		patchIsDead()
 	end
 end, Hook.HookMethodType.Before)
-
-Hook.Patch("Barotrauma.Character", "get_IsDead", function (instance, ptable)
-	if temporarilyUndeadCharacter ~= nil and temporarilyUndeadCharacter == instance then
-		ptable.PreventExecution = true
-		return false
-	end
-end, Hook.HookMethodType.Before)
-
 Hook.Patch("Barotrauma.CharacterHealth", "OnItemDropped", function (instance, ptable)
 	if temporarilyUndeadCharacter ~= nil then
 		temporarilyUndeadCharacter = nil
+		Hook.RemovePatch("NTC.IsDead_Patch", "Barotrauma.Character", "get_IsDead", Hook.HookMethodType.Before)
 	end
 end, Hook.HookMethodType.After)
